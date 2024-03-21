f8fa8d3f2715ea6b55a1b8baae263311047b3bd5
Adilkhan Nurgaliyev <ANurgaliyev@one.kz>
2024-03-21 12:37:32 +0500

    small fix

--- src/models/salaryModel.js
+++ src/models/salaryModel.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose")
 const Location = require("./locationModel")
 const Specialization = require("./specializationModel")
+const Company = require("./companyModel")
 
 const salarySchema = new mongoose.Schema({
     email: String,
     location: Location.schema,
     specialization: Specialization.schema,
+    company: Company.schema,
     salary: {
         base: Number,
         bonus: Number,
